71231643abd6d7c99e054f74acf7517837da2f7e
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2020-01-30 13:03:01 +0100

    Refactor: remove magic numbers

--- events.py
+++ events.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import os
 
-from evdev import InputDevice, ecodes
+from evdev import InputDevice, ecodes, events
 dev = InputDevice('/dev/input/event15')
 
 with dev.grab_context():
     for event in dev.read_loop():
-        if (event.code, event.value) == (ecodes.KEY_PAGEDOWN, 1):
+        if (event.code, event.value) == (ecodes.KEY_PAGEDOWN, events.KeyEvent.key_down):
             os.system('~/bin/page-turner next')
-        elif (event.code, event.value) == (ecodes.KEY_PAGEUP, 1):
+        elif (event.code, event.value) == (ecodes.KEY_PAGEUP, events.KeyEvent.key_down):
             os.system('~/bin/page-turner prior')
